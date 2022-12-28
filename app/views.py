20a2ff81ccba39f48a5255964a55214db1af7ad4
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-28 16:41:01 +0700

    feat: ui templates and view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def wordCounter(request):
+    __text = ""
+    __characters = 0
+    __words = 0
+    __sentences = 0
+    __paragraphs = 0
+    __readingTime = 0
+        
+    if request.method == 'POST':
+        __text = request.POST.get('textInput')
+
+    return render(request,'app/index.html')
